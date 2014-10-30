fe0444b4301a53119fa5ded44351829c263d5a5c
Rahul Desai <rahuldesai209@gmail.com>
2014-10-30 18:10:52 +0530

    integrated redis and added offline chat support

--- app.js
+++ app.js
@@ -1,52 +1,61 @@
 var app = require('express')();
 var server = require('http').Server(app);
+
 var io = require('socket.io')(server);
 
+var redis = require('redis');
+var redisClient = redis.createClient();
+
 server.listen(8080);
+
+var messages = [];
+
+var storeMessage = function(nickname, message){
+
+    var jsonString = JSON.stringify({nickname: nickname, message: message});
+
+    redisClient.lpush("messages", jsonString, function(error, response){
+        redisClient.ltrim("messages", 0, 3);
+    });
+};
 
 app.get('/', function (req, res) {
 	res.sendFile(__dirname + '/index.html');
 });
 
 io.on('connection', function (client) {
-
-	//var nickname_global;
 	
 	client.on('join', function(nickname){
 		client.nickname = nickname;
 		console.log(client.nickname + " has joined!");
-		client.broadcast.emit("messages", "<strong>" + client.nickname + "</strong> has joined!");
-		//var clients = findClientsSocket();
-		//console.log(clients);
+		client.broadcast.emit("messages", "<strong>" + client.nickname + "</strong> has joined!");        
 
-        var rooms = io.sockets.adapter.rooms;
-        var roomId = Object.keys(rooms);
-        console.log(roomId[0]);
+        // load earlier messages
+        redisClient.lrange("messages", 0, -1, function(error, messages){
+            messages = messages.reverse();  // so that it is in correct order
 
-        var clients = io.sockets.adapter.rooms[roomId[0]];
+            // notify that it is loading
+            if(messages.length === 1)
+                client.emit("messages", "<span class='italic'>Loading recent 1 message.</span>"); // eliminate the s in message(s)
+            else if(messages.length !== 0)
+                client.emit("messages", "<span class='italic'>Loading recent " + messages.length + " messages.</span>");
 
-        for (var clientId in clients ) {
+            messages.forEach(function(message){
+                message = JSON.parse(message);
+                client.emit("messages", "<strong>" + message.nickname + ":</strong> " + message.message);
+            });
 
-             //this is the socket of each client in the room.
-             var clientSocket = io.sockets.connected[clientId];
+            client.emit("messages", "<span class='italic'><strong>" + client.nickname + "</strong> has joined!</span>"); // let the user know
+        });
 
-             console.dir(clientSocket);
+        // give connection message
+        //client.emit("messages", "<p class='italic'>You have joined THE chatroom.</p>");
 
-        }
 	});
 	
 	client.on('messages', function (messages) {
 		client.broadcast.emit("messages", "<strong>" + client.nickname + ":</strong> " + messages);
+
+        storeMessage(client.nickname, messages);
 	});
 });
-
-function findClientsSocketByRoomId(roomId) {
-    var res = []
-    , room = io.sockets.adapter.rooms[roomId];
-    if (room) {
-        for (var id in room) {
-        res.push(io.sockets.adapter.nsp.connected[id]);
-        }
-    }
-    return res;
-}
