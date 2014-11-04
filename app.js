8d91359ac99845ba319e75aae289e3ec63741b6c
Rahul Desai <rahuldesai209@gmail.com>
2014-11-04 17:47:39 +0530

    added chatters list

--- app.js
+++ app.js
@@ -1,4 +1,6 @@
-var app = require('express')();
+var express = require('express');
+var app = express();
+
 var server = require('http').Server(app);
 
 var io = require('socket.io')(server);
@@ -20,15 +22,24 @@ var storeMessage = function(nickname, message){
 };
 
 app.get('/', function (req, res) {
-	res.sendFile(__dirname + '/index.html');
+    res.sendFile(__dirname + '/index.html');
 });
 
 io.on('connection', function (client) {
-	
-	client.on('join', function(nickname){
-		client.nickname = nickname;
-		console.log(client.nickname + " has joined!");
-		client.broadcast.emit("messages", "<strong>" + client.nickname + "</strong> has joined!");        
+    
+    client.on('join', function(nickname){
+        client.nickname = nickname;
+        console.log(client.nickname + " has joined!");
+        client.broadcast.emit("messages", "<span class='italic'><strong>" + client.nickname + "</strong> has joined!</span>");
+        client.broadcast.emit("add chatter", nickname);
+
+        redisClient.smembers('chatters', function(error, nicknames){
+            nicknames.forEach(function(nickname){
+                client.emit("add chatter", nickname);
+            })
+        });
+
+        redisClient.sadd("new_chatters", nickname);
 
         // load earlier messages
         redisClient.lrange("messages", 0, -1, function(error, messages){
@@ -51,11 +62,17 @@ io.on('connection', function (client) {
         // give connection message
         //client.emit("messages", "<p class='italic'>You have joined THE chatroom.</p>");
 
-	});
-	
-	client.on('messages', function (messages) {
-		client.broadcast.emit("messages", "<strong>" + client.nickname + ":</strong> " + messages);
+    });
+    
+    client.on('messages', function (messages) {
+        client.broadcast.emit("messages", "<strong>" + client.nickname + ":</strong> " + messages);
 
         storeMessage(client.nickname, messages);
-	});
+    });
+
+    client.on('disconnect', function(nickname){
+        console.log(client.nickname + " has disconnected.");
+        client.broadcast.emit("remove chatter", client.nickname);
+        redisClient.srem("new_chatters", client.nickname);        
+    });
 });
